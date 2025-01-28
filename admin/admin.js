b090b08a5d8e528b45e957bdbd8404f5c3816c06
spd12345 <64862751+spd12345@users.noreply.github.com>
2025-01-28 22:39:26 +0530

    28.01.2025 side bar added

--- admin/admin.js
+++ admin/admin.js
@@ -13,6 +13,37 @@ if( user == guser && pass == gpass ){
 }
 else{
     alert('invalid Input');
-    window.location.href = "/";
+    window.location.href = "admin.html";
 }
+}
+// addexperence
+function addexp(){
+    document.getElementById("hero").innerHTML =
+    "<h1> Add Experence is still under devlopment...</h1>";
+}
+function updresume(){
+    document.getElementById("hero").innerHTML =
+    "<h1> Update resume is still under devlopment...</h1>";
+}
+function updemail(){
+    document.getElementById("hero").innerHTML =
+    `<h1> Update Email | {still under devlopment}</h1><br>
+    <form action="">
+        <input type="email" placeholder="Enter updated Email" style = "width: 50vh; height: 5vh; border-radius: 5px; font-size: 15px; padding: 6px;" >
+            <input type="submit" onclick="emailSubmit()" style="height: 7vh; border-radius: 5px; font-size: 15px; color: #fff; transition: all 0.6s ease in-out; background-color:rgb(32, 153, 204); cursor: pointer;">
+    </form>
+     
+</body>`;
+}
+function profpic(){
+    document.getElementById("hero").innerHTML =
+    "<h1> profile pic is still under devlopment...</h1>";
+}
+function addskill(){
+    document.getElementById("hero").innerHTML =
+    "<h1> add skill is still under devlopment...</h1>";
+}
+function addproj(){
+    document.getElementById("hero").innerHTML =
+    "<h1> Add Project is still under devlopment...</h1>";
 }
